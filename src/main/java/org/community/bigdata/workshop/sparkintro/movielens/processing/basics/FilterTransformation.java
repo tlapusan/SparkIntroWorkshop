5b984a750d15e836f160ddece581cf27d3f471c6
tlapusan <tudor.lapusan@gmail.com>
2015-12-08 07:57:13 +0200

    Added docs for basics classes and the .gitignore file

--- src/main/java/org/community/bigdata/workshop/sparkintro/movielens/processing/basics/FilterTransformation.java
+++ src/main/java/org/community/bigdata/workshop/sparkintro/movielens/processing/basics/FilterTransformation.java
@@ -8,23 +8,34 @@ import org.community.bigdata.workshop.sparkintro.movielens.functions.conversion.
 import org.community.bigdata.workshop.sparkintro.movielens.model.User;
 
 /**
+ * This class illustrates Spark filter transformation and few Spark actions
+ * <p/>
  * Created by tudor on 12/7/2015.
  */
 public class FilterTransformation {
     public static void main(String[] args) {
+
+        // initialize spark configuration and context
         SparkConf conf = new SparkConf().setMaster("local[4]").setAppName(FilterTransformation.class.getSimpleName());
         conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
+        // read 'users' file line by line and each string line is converted into a User object
         JavaRDD<User> userJavaRDD = sc.textFile("data/movielens/input/users").map(new UserConversion());
+
+        // return a new RDD made from only the Users for each function call return true
         JavaRDD<User> administrators = userJavaRDD.filter(new Function<User, Boolean>() {
             public Boolean call(User user) throws Exception {
                 return user.getOccupation().equals("administrator");
             }
         });
+
+        // return the administrators RDD as a list to the driver program and display it to console
         for (User user : administrators.collect()) {
             System.out.println(user);
         }
+
+        // display the total number of administrators
         System.out.println(administrators.count());
     }
 }
--- src/main/java/org/community/bigdata/workshop/sparkintro/movielens/processing/basics/MapTransformation.java
+++ src/main/java/org/community/bigdata/workshop/sparkintro/movielens/processing/basics/MapTransformation.java
@@ -8,15 +8,24 @@ import org.community.bigdata.workshop.sparkintro.movielens.functions.conversion.
 import org.community.bigdata.workshop.sparkintro.movielens.model.User;
 
 /**
+ * This class illustrates Spark map transformation and few Spark actions
  * Created by tudor on 12/7/2015.
  */
 public class MapTransformation {
     public static void main(String[] args) {
+
+        // initialize spark configuration and context
         SparkConf conf = new SparkConf().setMaster("local[4]").setAppName(FilterTransformation.class.getSimpleName());
         conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<User> userJavaRDD = sc.textFile("data/movielens/input/users").map(new UserConversion());
+        // read 'users' file line by line
+        JavaRDD<String> records = sc.textFile("data/movielens/input/users");
+
+        // go through each string line and return a new RDD of User objects
+        JavaRDD<User> userJavaRDD = records.map(new UserConversion());
+
+        // go through each user and increase its age by one
         JavaRDD<User> userJavaRDDMap = userJavaRDD.map(new Function<User, User>() {
             public User call(User user) throws Exception {
                 user.setAge(user.getAge() + 1);
@@ -24,8 +33,11 @@ public class MapTransformation {
             }
         });
 
+        // return the first element from RDD
         System.out.println(userJavaRDD.first());
         System.out.println(userJavaRDDMap.first());
+
+        // take n elements from RDD and bring them to main class (driver program)
         for (User user : userJavaRDD.take(10)) {
             System.out.println(user);
         }
